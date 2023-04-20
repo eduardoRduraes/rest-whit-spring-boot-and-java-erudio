e225c61fcfe205180a735012a1b6e1b8e294c8e6
eduardoRduraes <eduardoduraes.bsi@gmail.com>
2023-04-20 14:38:26 -0300

    first commit

---
+++ src/main/java/br/com/erudio/restwhitspringbootandjavaerudio/RestWhitSpringBootAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwhitspringbootandjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWhitSpringBootAndJavaErudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWhitSpringBootAndJavaErudioApplication.class, args);
+    }
+
+}
