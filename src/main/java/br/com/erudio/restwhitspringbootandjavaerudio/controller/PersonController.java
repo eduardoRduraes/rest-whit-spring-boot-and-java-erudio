52465f8242b4653e5a0650be2cdaa5e419c99f0b
eduardoRduraes <eduardoduraes.bsi@gmail.com>
2023-04-25 13:58:19 -0300

    commit final 07 curso

--- src/main/java/br/com/erudio/restwhitspringbootandjavaerudio/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwhitspringbootandjavaerudio/controller/PersonController.java
@@ -7,14 +7,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonService service;
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Person create(@RequestBody Person person){
+        return service.create(person);
+    }
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public Person findById(@PathVariable String id){
+    public Person findById(@PathVariable(value = "id") String id){
         return service.findById(id);
+    }
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Person> findALl(){
+        return service.findAll();
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Person update(@RequestBody Person person, @PathVariable(value = "id") String id){
+        return service.update(person, id);
+    }
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable(value="id") String id){
+        service.delete(id);
     }
 }
--- src/main/java/br/com/erudio/restwhitspringbootandjavaerudio/service/PersonService.java
+++ src/main/java/br/com/erudio/restwhitspringbootandjavaerudio/service/PersonService.java
@@ -3,6 +3,8 @@ package br.com.erudio.restwhitspringbootandjavaerudio.service;
 import br.com.erudio.restwhitspringbootandjavaerudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,14 +14,51 @@ public class PersonService {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
-    public Person findById(String id){
+    public Person create(Person person){
         logger.info("Finding one person!");
-        Person person =  new Person();
+        person.setId(counter.incrementAndGet());
+        return person;
+    }
+
+    public Person findById(String id) {
+        logger.info("Finding one person!");
+        Person person = new Person();
         person.setId(counter.incrementAndGet());
         person.setFirstName("firstName");
         person.setLastName("lastName");
         person.setAddress("Rua nomeRua, numeroRua, bairroRua");
         person.setGender("Masculino");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+        logger.info("Finding one person!");
+        for (int i = 0; i < 10; i++) {
+            Person person = mockListPerson(i);
+
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person update(Person person, String id){
+        logger.info("Finding one person!");
+        person.setId(Long.parseLong(id));
+        return person;
+    }
+
+    public void delete(String id){
+        logger.info("delete one person!");
+    }
+
+    private Person mockListPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("firstName");
+        person.setLastName("lastName "+i);
+        person.setAddress("Rua nomeRua, numeroRua, bairroRua");
+        person.setGender(i%2 == 0 ? "Masculino" : "Feminino");
         return person;
     }
 }
