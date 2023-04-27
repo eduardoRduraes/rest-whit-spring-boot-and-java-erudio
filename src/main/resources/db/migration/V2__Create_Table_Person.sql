b409e9aa1c7d7e6f61ab50431f58be6279819985
eduardoRduraes <eduardoduraes.bsi@gmail.com>
2023-04-27 10:16:46 -0300

    firt final secao-11

---
+++ src/main/resources/db/migration/V2__Create_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO persons (first_name, last_name, address, gender)
+VALUES ('João', 'Silva', 'Rua das Flores, 123', 'Masculino'),
+       ('Maria', 'Santos', 'Av. Paulista, 1000', 'Feminino'),
+       ('Pedro', 'Almeida', 'Rua do Sol, 456', 'Masculino'),
+       ('Ana', 'Sousa', 'Rua dos Pinheiros, 789', 'Feminino'),
+       ('Carlos', 'Pereira', 'Av. Brasil, 2000', 'Masculino'),
+       ('Paulo', 'Gomes', 'Av. Valadares, 400', 'Masculino');
